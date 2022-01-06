302a7186f69311ef6ca3566a081bbee081ad33e8
Eric Baker <eric100596@aol.com>
2022-01-05 20:21:34 -0600

    jasmine

---
+++ spec/helloWorld.spec.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+
+const helloWorld = require('../helloWorld.js');
+
+describe("helloWorld()", function() {
+
+//Arrange
+let greeting = 'Hello World';
+
+//Act
+let result = helloWorld();
+
+//Assert
+it("it should return Hello World", function(){
+  expect(result).toEqual(greeting);
+});
+});
